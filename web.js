9e0fd92eb5a669759d9f20cdf16ec4cdd8acf957
Bryan Glazer <bglazer1@gmail.com>
2013-08-10 19:31:40 +0000

    From index.html now

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+index_buffer = fs.readFileSync('index.html');
+index = Buffer.toString(index_buffer);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
